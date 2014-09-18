18ea5c010c2b15de58d33b810d25859f531445b7
hnsz <noemail@sorry.com>
2014-09-18 21:37:19 +0200

    Added a skel for generic feistel implementation

---
+++ feistel.c
@@ -0,0 +1,21 @@
+/*******
+*	feistel.c
+*/
+
+
+/*
+	out: encrypted/decrypted ciphertext/message
+	in:  message/ciphertext
+	blocksize: size of the blocks
+	key:	key
+	F:	round function of the encryption
+	K:	function that contains the key scheme algorithm
+	
+*/
+void feistel(char *out, char *in, int blocksize, char *key, void (F)(char *, char *), void (*KS)(char *,char *))
+{
+	//	placeholder, for x number of rounds do
+	//	Lnew = R
+	//	Rnew = L xor F(R,KS(key))
+	//	where KS(key) is the current iteration of the key scheme
+}
